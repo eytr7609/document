286410ddfd5e5de865efbe750d3f44c8a0e82267
eytr7609 <eytr7609@gmail.com>
2018-05-29 13:31:14 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import os
-
+import time
 
 with open('train.json', 'r') as reader:
 	jf = json.loads(reader.read())
@@ -13,9 +13,15 @@ path = os.getcwd()
 if not os.path.isdir(path + '/train_image'):
 	os.mkdir("train_image")
 os.chdir(path + '/train_image')
+start = input("start: ")
+end = input("end: ")
 
-for i in range(0,10):
-	print(jf['images'][i]['image_id'])
-	r = requests.get(jf['images'][i]['url'][0])
-	with open(str(jf['images'][i]['image_id']) + '.jpg','wb') as f:
-		f.write(r.content)
+for i in range(int(start), int(end)):#len(jf['images']) - 1):
+	try:
+		r = requests.get(jf['images'][i]['url'][0])
+		with open(str(jf['images'][i]['image_id']) + '.jpg','wb') as f:
+			f.write(r.content)
+	except:
+		print(jf['images'][i]['image_id'],"error")
+	time.sleep(1)
+
